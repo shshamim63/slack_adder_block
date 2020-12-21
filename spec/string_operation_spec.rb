f7513a6cac0b020f4b626716eb2622416f7dabd3
shshamim63 <shossain142056@bscse.uiu.ac.bd>
2020-12-21 21:45:26 +0600

    Added test for testing separate number generation from the string.

--- spec/string_operation_spec.rb
+++ spec/string_operation_spec.rb
@@ -12,4 +12,14 @@ RSpec.describe Calculation do
       expect(Calculation.seperate_target_string(command_string)).to eql("123+123")
     end
   end
+
+  context 'separate_numbers' do
+    it "returns the two separate numbers in string format enclosed in an array(+ is pattern)" do
+      expect(Calculation.separate_numbers(command_string)).to eql(["123","123"])
+    end
+
+    it "returns the two separate numbers in string format enclosed in an array(+ is pattern)" do
+      expect(Calculation.separate_numbers(command_converted_string)).not_to eql(["123","123"])
+    end
+  end
 end
